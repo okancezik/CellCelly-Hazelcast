418e3672e71b2b9854fa39e7a0b044d381100d0d
Okan Cezik <okacezik@gmail.com>
2023-08-07 17:48:41 +0300

    added Hazelcast simulator operations

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,22 +1,26 @@
 package org.example;
 
 import org.example.hazelcast_operations.HazelcastDGWOperation;
+import org.example.hazelcast_operations.HazelcastMWOperation;
+import org.example.hazelcast_operations.HazelcastSimulatorOperation;
 
 import java.util.Collection;
 
 public class Main {
     public static void main(String[] args) {
 
-        //String result = HazelcastDGWOperation.getPartitionIdByKey("Second");
+        //String result = HazelcastDGWOperation.getPartitionIdByKey("Fourht");
         //System.out.println(result);
 
-        Collection<Object> all = HazelcastDGWOperation.getAllPartitionId();
+        Collection<Object> all = HazelcastSimulatorOperation.getAllPartitionId();
         //System.out.println(all.toArray()[0]);
         System.out.println(all);
-       //String result = HazelcastPutOperation.put("Third","Rıfkı");
-       //System.out.println(result);
-
-        //String result = HazelcastPutOperation.remove("Firstasada");
+        //String result = HazelcastMWOperation.put("First","Okan");
         //System.out.println(result);
+
+        //String result = HazelcastMWOperation.remove("Second");
+        //System.out.println(result);
+
+
     }
 }
--- src/main/java/org/example/hazelcast_operations/HazelcastSimulatorOperation.java
+++ src/main/java/org/example/hazelcast_operations/HazelcastSimulatorOperation.java
@@ -4,32 +4,27 @@ import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.client.config.ClientConfig;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.map.IMap;
-
 import org.example.utils.configurations.Configuration;
 import org.example.utils.constants.StringConstants;
 
 import java.util.Collection;
 
-public class HazelcastDGWOperation {
+public class HazelcastSimulatorOperation {
     private static final ClientConfig config = Configuration.getConfig();
     private static final HazelcastInstance hazelcast = HazelcastClient.newHazelcastClient(config);
 
-    public static String getPartitionIdByKey(String key) {
-        try{
+    public static Collection<Object> getAllPartitionId() {
+        try {
             IMap<Object, Object> map = hazelcast.getMap(StringConstants.mapName);
 
-            if (map.containsKey(key)) {
-                String value = map.get(key).toString();
-                return value;
-            } else {
-                return "Not found key";
-            }
-        }catch (Exception e){
-            return e.toString();
-        }finally {
+            Collection<Object> allPartitionId = map.values();
+
+            return allPartitionId;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        } finally {
             hazelcast.shutdown();
         }
-
     }
-
 }
